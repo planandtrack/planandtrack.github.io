8f790ed57d9ef9efd7086296dd03a865be086cd2
Philipp Hoffmann <ph.hoffmann@tum.de>
2016-01-21 17:12:05 +0100

    add settings saving

--- js/planandtrack.js
+++ js/planandtrack.js
@@ -1,3 +1,5 @@
+var settingsID;
+
 function loadSettings() {
 	deferUntilGDrive(function() {
 		var request = gapi.client.drive.files.list();
@@ -43,6 +45,7 @@ function syncSettingsWithCalendar(settings) {
 }
 
 function loadSettingsFile(fileID) {
+	settingsID = filesID;
 	var request = gapi.client.drive.files.get({fileId: fileID,alt:"media"});
 	request.execute(function(resp) {
 		addSettingsToAngular(resp);
@@ -50,9 +53,53 @@ function loadSettingsFile(fileID) {
 	});
 }
 
+function saveSettings() {
+	var scope = angular.element(document.getElementById('contentCtrl')).scope();
+	var settings = sc.ope.calendarSettings;
+	updateFileWithJSONContent(settingsID,'settings',JSON.stringify(settings),null);
+}
+
 function createSettings() {
 	createFileWithJSONContent('settings','{"calendars":{}}',function(resp) {loadSettingsFile(resp.id)})
 }
+
+var updateFileWithJSONContent = function(id, name, data, callback) {
+  const boundary = '-------314159265358979323846';
+  const delimiter = "\r\n--" + boundary + "\r\n";
+  const close_delim = "\r\n--" + boundary + "--";
+  
+  const contentType = 'application/json';
+
+  	var metadata = {
+      	'name': name,
+      	'mimeType': contentType
+    };
+
+    var multipartRequestBody =
+        delimiter +
+        'Content-Type: application/json\r\n\r\n' +
+        JSON.stringify(metadata) +
+        delimiter +
+        'Content-Type: ' + contentType + '\r\n\r\n' +
+        data +
+        close_delim;
+
+    var request = gapi.client.request({
+        'path': '/upload/drive/v3/files/'+id,
+        'method': 'POST',
+        'params': {'uploadType': 'multipart'},
+        'headers': {
+          'Content-Type': 'multipart/related; boundary="' + boundary + '"'
+        },
+        'body': multipartRequestBody});
+    if (!callback) {
+      callback = function(file) {
+        console.log(file)
+      };
+    }
+    request.execute(callback);
+}
+
 
 var createFileWithJSONContent = function(name,data,callback) {
   const boundary = '-------314159265358979323846';
